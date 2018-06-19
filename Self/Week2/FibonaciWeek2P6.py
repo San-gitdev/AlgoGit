95d3b0d467a18224ff462730fa5035e83013500d
Sandeep Kulkarni <Sandeep.Kulkarni@continuum.net>
2018-06-19 22:03:49 +0530

    Fibo week2 Program 6

--- Self/Week2/FibonaciWeek2P6.py
+++ Self/Week2/FibonaciWeek2P6.py
@@ -3,15 +3,21 @@
 # Input Format. The input consists of a single integer 𝑛.
 #Constraints. 0 ≤ 𝑛 ≤ 1018.
 #Output Format. Output the last digit of 𝐹0 + 𝐹1 + · · · + 𝐹𝑛.
+# Remarks - Sum of first x number is equal to fibonacci number (x+2)
 
 def LastDigitofsummingnFibos(n):
     i=0
     j=1
     sum=i+j
-    for cnt in range(2, n+1):
+    for _ in range(2, n+2):
         i,j=j,i+j
-        sum=sum+j
-        print("Fibonacci number ", cnt, " is ", j," and its sum is " , sum)
+        sum=i+j
+        i,j,sum=i%10,j%10,sum%10
+        if(sum==0):
+            sum=10
+        #print(i,j,sum-1)
+       
+    print("Last digit of Sum of first ", n, " Fibonacci numbers is: ",  sum-1)
     return
 
 
